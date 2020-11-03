6b482cc4e8a4578e59d37b2f857ef508c670ef30
umeneri <koishimst@gmail.com>
2020-11-03 19:40:52 +0900

    add spec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit :sign_up, keys: [:name]
+    devise_parameter_sanitizer.permit :sign_in, keys: [:name]
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
   end
 end
