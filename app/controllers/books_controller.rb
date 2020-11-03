f1eea982a6fe66a4e7a711832010f3f29cfc9c52
umeneri <koishimst@gmail.com>
2020-11-03 18:59:56 +0900

    add flush

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to books_path
+      redirect_to books_path, notice: 'created successfully'
     else
       render :new
     end
@@ -28,7 +28,8 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice: 'updated successfully'
+
     else
       render :edit
     end
@@ -37,10 +38,8 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_path
+    redirect_to books_path, notice: 'destroyed successfully'
   end
-
-  private
 
   def book_params
     params.require(:book).permit(:title, :body)
