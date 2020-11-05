77f86ff556d5936fc830cede8acafdadf3cfedee
umeneri <koishimst@gmail.com>
2020-11-05 22:58:51 +0900

    Fix: registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit :sign_up, keys: [:name]
-    devise_parameter_sanitizer.permit :sign_in, keys: [:name]
+    devise_parameter_sanitizer.permit :sign_up, keys: [:name, :email]
+    devise_parameter_sanitizer.permit :sign_in, keys: [:name, :email]
   end
 
   def after_sign_in_path_for(resource)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
+    params.require(:user).permit(:name, :email, :introduction, :profile_image)
   end
 end
