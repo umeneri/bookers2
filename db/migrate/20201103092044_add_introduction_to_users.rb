dd6d81d1b9b40c8b8cece71693462419b7c9afe3
umeneri <koishimst@gmail.com>
2020-11-08 22:41:32 +0900

    Fix: db migration

--- db/migrate/20201103092044_add_introduction_to_users.rb
+++ db/migrate/20201103092044_add_introduction_to_users.rb
@@ -1,5 +1,5 @@
 class AddIntroductionToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :introduction, :text, null: false, default: ''
+    add_column :users, :introduction, :string, null: false, default: ''
   end
 end
