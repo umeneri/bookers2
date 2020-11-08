f30969b77a2bd8a7bc9beae421380725223b27fb
umeneri <koishimst@gmail.com>
2020-11-08 22:54:42 +0900

    Fix: refile secret key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ed8944ca15d4f07548a8bcaf0880e2c63651497848fab87d923aa1f7855d89a6ba66b6f7370da673494f70baf7836d9ea7e4ca22f11e6331d270fcf2ea14e288'
